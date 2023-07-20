98b320ce36656a6bf137d5c71ac7cb764fbb24fd
Unicode <73183149+NicolasIDias@users.noreply.github.com>
2023-07-20 19:58:19 -0300

    Update and rename basic_search.cpp to basic_search.c

--- Data structure & algorithms/basic_search.c
+++ Data structure & algorithms/basic_search.c
@@ -1,21 +1,19 @@
-#include <iostream>
 #include <stdlib.h>
 
 #include <stdio.h>
-using namespace std;
 
 int main(){
     int array[10] = {1,2,3,4,5,6,7,8,9,10};
     int search;
     int position;
-    bool valueFound = false;
+    int valueFound = -1;
 
-    cout << "Number to search: ";
+    printf("Number to search: ");
     scanf("%i", &search);
 
     for(int i=0;i<sizeof(array)/4;i++){
         if(search == array[i]){
-            valueFound = true;
+            valueFound = 1;
             position = i;
         }
     }
